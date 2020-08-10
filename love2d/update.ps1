d80487ec5cffda81b1d86cec324d5421a1c3f0a5
Syrus Dark <naveen@syrusdark.website>
2020-08-10 22:10:09 +0530

    Add love2d package

---
+++ love2d/update.ps1
@@ -0,0 +1,99 @@
+import-module au
+
+#for mysql python connector
+$releases = 'https://github.com/love2d/love/releases/'
+$github_api_url='https://api.github.com/repos/love2d/love/releases'
+
+$Options = [ordered]@{
+  Timeout = 100
+  Threads = 15
+  Push    = $true
+    
+  # Save text report in the local file report.txt
+  Report = @{
+      Type = 'text'
+      Path = "$PSScriptRoot\report.txt"
+  }
+  
+  # Then save this report as a gist using your api key and gist id
+  Gist = @{
+      ApiKey = $Env:github_api_key
+      Id     = $Env:github_gist_id
+      Path   = "$PSScriptRoot\report.txt"
+  }
+
+  # Persist pushed packages to your repository
+  Git = @{
+      User = 'naveen521kk'
+      Password = $Env:github_api_key
+  }
+  
+  # Then save run info which can be loaded with Import-CliXML and inspected
+  RunInfo = @{
+      Path = "$PSScriptRoot\update_info.xml"
+  }
+
+  # Finally, send an email to the user if any error occurs and attach previously created run info
+  Mail = if ($Env:mail_user) {
+          @{
+             To          = $Env:mail_user
+             Server      = 'smtp.gmail.com'
+             UserName    = $Env:mail_user
+             Password    = $Env:mail_pass
+             Port        = 587
+             EnableSsl   = $true
+             Attachment  = "$PSScriptRoot\$update_info.xml"
+             UserMessage = 'Save attachment and load it for detailed inspection: <code>$info = Import-CliXCML update_info.xml</code>'
+          }
+  } else {}
+}
+function global:au_GetLatest {
+  $contentFetched = Invoke-WebRequest $github_api_url | ConvertFrom-Json
+  $i=0
+  $stableRelease=$contentFetched[0]
+  while ($j[$i].prerelease -eq $true){
+    $stableRelease=$j[$i+1]
+    $i=$i+1
+  }
+  $regex32 = '.*love-.*-win32.exe'
+  $regex64 = '.*love-.*-win64.exe'
+  foreach ($asset in $stableRelease.assets){
+    if (!($url64)){
+       $url64   = Select-String -InputObject $asset.browser_download_url -Pattern $regex64
+       $id64 = $asset.id
+    }
+    if (!($url32)){
+       $url32   = Select-String -InputObject $asset.browser_download_url -Pattern $regex32
+       $id32 = $asset.id
+      }
+    if ($url64 -and $url32){
+      break
+    }
+  }
+  $version = $stableRelease.tag_name
+  return @{ Version = $version; URL64 = $url64; URL32 = $url32;}
+}
+
+#todo from here
+function global:au_SearchReplace {
+  @{
+    ".\tools\VERIFICATION.txt"      = @{
+      "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$releases>"
+      "(?i)(\s*64\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL64)>"
+      "(?i)(\s*32\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL32)>"
+      "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType64)"
+      "(?i)(^\s*checksum32\:).*"          = "`${1} $($Latest.Checksum32)"
+      "(?i)(^\s*checksum64\:).*"          = "`${1} $($Latest.Checksum64)"
+    }
+    ".\tools\chocolateyinstall.ps1" = @{
+      "(?i)(^\s*Url\s*=\s*)'.*'"                  = "`${1}'$($Latest.URL32)'"
+      "(?i)(^\s*Url64bit\s*=\s*)'.*'"             = "`${1}'$($Latest.URL64)'"
+      "(?i)(^\s*Checksum\s*=\s*)'.*'"             = "`${1}'$($Latest.Checksum32)'"
+      "(?i)(^\s*Checksum64\s*=\s*)'.*'"           = "`${1}'$($Latest.Checksum64)'"
+      "(?i)(^\s*ChecksumType\s*=\s*)'.*'"         = "`${1}'$($Latest.ChecksumType32)'"
+      "(?i)(^\s*ChecksumType64\s*=\s*)'.*'"       = "`${1}'$($Latest.ChecksumType64)'"
+    }
+  }
+}
+
+update
