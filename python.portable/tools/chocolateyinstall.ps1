18b1589069176dda9d82d2078dccd5ddce2d1b6f
Syrus Dark <naveen@syrusdark.website>
2020-08-17 12:54:33 +0530

    Update 32 bit

--- python.portable/tools/chocolateyinstall.ps1
+++ python.portable/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 $version = '3.8.5.20200816'.SubString(0,5)
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$osBitness = Get-ProcessorBits
-if ( $osBitness -eq 32 -or $env:ChocolateyForceX86 -eq "true" ){
+$osBitness = Get-OSArchitectureWidth -Compare 32 -or $env:ChocolateyForceX86 -eq "true"
+if ( $osBitness ){
   nuget install pythonx86 -Version $version -OutputDirectory "$toolsDir\Python" -Verbosity "quiet"
 } else {
   nuget install python -Version $version -OutputDirectory "$toolsDir\Python" -Verbosity "quiet"
