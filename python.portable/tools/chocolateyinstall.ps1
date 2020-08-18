389d02e8683931296ebe1149442d4a77cb92e63c
Syrus Dark <naveen@syrusdark.website>
2020-08-18 13:13:11 +0530

    Test Python.portable

--- python.portable/tools/chocolateyinstall.ps1
+++ python.portable/tools/chocolateyinstall.ps1
@@ -2,8 +2,14 @@
 $version = '3.8.5.20200816'.SubString(0,5)
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $osBitness = Get-OSArchitectureWidth -Compare 32 -or $env:ChocolateyForceX86 -eq "true"
+$nugetUrl="https://dist.nuget.org/win-x86-commandline/v5.8.0-preview.1/nuget.exe"
+$nugetLoc = Get-ChocolateyWebFile `
+  -PackageName "Nuget" `
+  -FileFullPath "$toolsDir\nuget1.exe" `
+  -Url $nugetUrl `
+  -Checksum "C73E10BA5ED0054C21C382D0A7F040E813C609918E7C1DC3AD601E629146BD2E"
 if ( $osBitness ){
-  nuget install pythonx86 -Version $version -OutputDirectory "$toolsDir\Python" -Verbosity "quiet"
+  & "$nugetLoc" install pythonx86 -Version $version -OutputDirectory "$toolsDir\Python" -Verbosity "quiet"
 } else {
-  nuget install python -Version $version -OutputDirectory "$toolsDir\Python" -Verbosity "quiet"
+  & "$nugetLoc" install python -Version $version -OutputDirectory "$toolsDir\Python" -Verbosity "quiet"
 }
