27eddecf5f61de9fa750f6768930d0ebece35b97
Syrus Dark <naveen@syrusdark.website>
2020-09-18 20:07:16 +0530

    Add Manim Tex

---
+++ manim-latex/tools/chocolateyinstall.ps1
@@ -0,0 +1,7 @@
+﻿$ErrorActionPreference = 'Stop'
+#tlmgr is package manager included in TinyTeX
+Start-ChocolateyProcessAsAdmin `
+  -Statements "install standalone preview doublestroke ms setspace rsfs relsize ragged2e fundus-calligra microtype wasysym physics dvisvgm jknapltx wasy cm-super babel-english" `
+  -ExeToRun "tlmgr.exe" `
+  -Elevated
+
---
+++ manim-latex/update.ps1
@@ -0,0 +1,20 @@
+import-module au
+
+#for tinytex
+$releases = 'https://github.com/yihui/tinytex-releases/releases'
+$github_api_url='https://api.github.com/repos/yihui/tinytex-releases/releases'
+
+function global:au_GetLatest {
+  $contentFetched = Invoke-WebRequest $github_api_url | ConvertFrom-Json
+  $i=0
+  $stableRelease=$contentFetched[0]
+  while ($contentFetched[$i].prerelease -eq $true){
+    $stableRelease=$j[$i+1]
+    $i=$i+1
+  }
+  $version = $stableRelease.tag_name
+  $version=$version.Substring(1)
+  return @{ Version = $version;}
+}
+
+update -ChecksumFor none
