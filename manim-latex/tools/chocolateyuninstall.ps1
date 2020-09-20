fbca3ff0790507ba698ca1cbe723da59c6c113dd
Syrus Dark <naveen@syrusdark.website>
2020-09-20 13:30:28 +0530

    Update installer for manim-latex to use cmd

--- manim-latex/tools/chocolateyuninstall.ps1
+++ manim-latex/tools/chocolateyuninstall.ps1
@@ -1,5 +1,5 @@
 ﻿$ErrorActionPreference = 'Stop';
 Start-ChocolateyProcessAsAdmin `
-  -Statements "remove standalone preview doublestroke ms setspace rsfs relsize ragged2e fundus-calligra microtype wasysym physics dvisvgm jknapltx wasy cm-super babel-english" `
-  -ExeToRun "tlmgr.exe" `
+  -Statements "/C 'tlmgr remove standalone preview doublestroke ms setspace rsfs relsize ragged2e fundus-calligra microtype wasysym physics dvisvgm jknapltx wasy cm-super babel-english'" `
+  -ExeToRun "cmd.exe" `
   -Elevated
