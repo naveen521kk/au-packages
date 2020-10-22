243d9ab42b081e6837b633901507f6ff51c14922
Syrus Dark <naveen@syrusdark.website>
2020-10-22 20:31:10 +0530

    move manim to this repo

---
+++ manim.install/tools/chocolateyuninstall.ps1
@@ -0,0 +1,7 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+$InstallLocation = Get-ToolsLocation
+. $toolsPath\helper.ps1
+Uninstall-ChocolateyPath "$InstallLocation\pango"
+python -m pip uninstall -y -q manimce
+
---
+++ manim.install/update.ps1
@@ -0,0 +1,28 @@
+import-module au
+
+$github_api_url = 'https://api.github.com/repos/manimcommunity/manim/releases'
+
+function global:au_GetLatest {
+  $contentFetched = Invoke-WebRequest $github_api_url | ConvertFrom-Json
+  $i = 0
+  $stableRelease = $contentFetched[0]
+  while ($contentFetched[$i].prerelease -eq $true) {
+    $stableRelease = $j[$i + 1]
+    $i = $i + 1
+  }
+  $version = $stableRelease.tag_name
+  return @{ Version = $version; }
+}
+
+function global:au_SearchReplace {
+  @{
+    ".\manim.install.nuspec"        = @{
+      "(?im)(<docsUrl>)(.*?)(<\/docsUrl>)" = "`${1}https://manimce.readthedocs.io/en/v$($Latest.Version)/`${3}"
+    }
+    ".\tools\chocolateyinstall.ps1" = @{
+      "(?i)(^.*version\s*=\s*)'.*'"    = "`${1}'$($Latest.Version)'"
+    }
+  }
+}
+
+update -ChecksumFor none
