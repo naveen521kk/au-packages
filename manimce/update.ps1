acff78ada7605b38e551ffb8966a1e9724acf2cf
Syrus Dark <naveen@syrusdark.website>
2020-10-23 21:03:02 +0530

    manimce: try fixing updator

--- manimce/update.ps1
+++ manimce/update.ps1
@@ -18,7 +18,7 @@ function global:au_GetLatest {
 function global:au_SearchReplace {
   @{
     ".\manim.install.nuspec"        = @{
-      "(?im)(<docsUrl>)(.*?)(<\/docsUrl>)" = "`${1}https://manimce.readthedocs.io/en/v$($Latest.Version)/`${3}"
+      "(?im)(<docsUrl>)(.*?)(<\/docsUrl>)" = "`${1}https://manimce.readthedocs.io/en/v$($Latest.Version)`${3}"
     }
     ".\tools\chocolateyinstall.ps1" = @{
       "(?i)(^.*version\s*=\s*)'.*'" = "`${1}'$($Latest.Version)'"
