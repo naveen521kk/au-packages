82ba0852153f097f7594a4b1d279c8377b0bfc41
Syrus Dark <naveen@syrusdark.website>
2020-11-18 19:20:37 +0530

    python-mysql: fix updator

--- mysql-python/update.ps1
+++ mysql-python/update.ps1
@@ -2,31 +2,20 @@ import-module au
 
 #for mysql python connector
 $releases = 'https://dev.mysql.com/downloads/connector/python/'
-$releasesx64 = 'https://dev.mysql.com/downloads/file/?id=496500'
-$releasesx32= 'https://dev.mysql.com/downloads/file/?id=496499'
 $baserelease = 'https://dev.mysql.com'
 
 function global:au_GetLatest {
-  $download_page = Invoke-WebRequest -Uri $releasesx64 -UseBasicParsing
-  $regex   = '.msi$'
-  $url64   = $download_page.links | ? href -match $regex | select -First 1 -expand href
-  $version = $url64 -split '-|.msi' | select -Last 1 -Skip 4
-  
-  $download_page = Invoke-WebRequest -Uri $releasesx32 -UseBasicParsing
-  $url32     = $download_page.links | ? href -match $regex | select -First 1 -expand href
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $regex   = 'mysql-connector-python-(?<version>\d*\.\d*\.\d*)-windows-x86-64bit.msi'
+  $match   = $download_page.Content -match $regex | select -First 1
+  $version = $Matches.version
+  $url64   = "https://dev.mysql.com/get/Downloads/Connector-Python/mysql-connector-python-$($version)-windows-x86-64bit.msi"
+  $url32     = "https://dev.mysql.com/get/Downloads/Connector-Python/mysql-connector-python-$($version)-windows-x86-32bit.msi"
   return @{ Version = $version; URL64 = $baserelease+$url64; URL32 = $baserelease+$url32;}
 }
 
 function global:au_SearchReplace {
   @{
-    ".\tools\VERIFICATION.txt"      = @{
-      "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$releases>"
-      "(?i)(\s*64\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL64)>"
-      "(?i)(\s*32\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL32)>"
-      "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType64)"
-      "(?i)(^\s*checksum32\:).*"          = "`${1} $($Latest.Checksum32)"
-      "(?i)(^\s*checksum64\:).*"          = "`${1} $($Latest.Checksum64)"
-    }
     ".\tools\chocolateyinstall.ps1" = @{
       "(?i)(^\s*Url\s*=\s*)'.*'"                  = "`${1}'$($Latest.URL32)'"
       "(?i)(^\s*Url64bit\s*=\s*)'.*'"             = "`${1}'$($Latest.URL64)'"
