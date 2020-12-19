8db53cb11f5154792ddb4904de94972d9ff99ab5
Naveen M K <naveen@syrusdark.website>
2020-12-19 09:10:55 +0530

    manimce: fix uninstall

--- manimce/tools/chocolateybeforemodify.ps1
+++ manimce/tools/chocolateybeforemodify.ps1
@@ -1,3 +1,6 @@
 ﻿$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $InstallLocation = Get-ToolsLocation
+Copy-Item -LiteralPath "$InstallLocation\Manim\installInfo.json" -Destination "$InstallLocation" -Force
 Remove-Item $InstallLocation\Manim -Recurse
+mkdir $InstallLocation\Manim
+Copy-Item -LiteralPath "$InstallLocation\installInfo.json" -Destination "$InstallLocation\Manim" -Force
