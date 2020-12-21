9cf5db4b712cbbd79d5bdc25c4aa193ca5af5a2a
Naveen M K <naveen@syrusdark.website>
2020-12-21 16:06:46 +0530

    manimce: Uninstall variable names also

--- manimce/tools/chocolateyuninstall.ps1
+++ manimce/tools/chocolateyuninstall.ps1
@@ -3,6 +3,18 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 $InstallLocation = Get-ToolsLocation
 . $toolsPath\helper.ps1
 Uninstall-ChocolateyPath "$InstallLocation\pango"
+
+Write-Host "Uninstalling Environment Variables" -ForegroundColor Red
+Uninstall-ChocolateyEnvironmentVariable `
+  -VariableName "PANGO_LOCATION" `
+  -VariableType Machine
+Uninstall-ChocolateyEnvironmentVariable `
+  -VariableName "GLIB_LOCATION" `
+  -VariableType Machine
+Uninstall-ChocolateyEnvironmentVariable `
+  -VariableName "GOBJECT_LOCATION" `
+  -VariableType Machine
+
 $installInfo = Get-Content "$InstallLocation\Manim\installInfo.json" | ConvertFrom-JSON
 Uninstall-BinFile `
   -Name "manim" `
