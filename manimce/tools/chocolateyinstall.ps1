3af4388e9c8e34cde6ae7afff6ab25273f07a26b
Naveen M K <naveen@syrusdark.website>
2021-03-03 19:47:30 +0530

    manimce: use reverse order for python version

--- manimce/tools/chocolateyinstall.ps1
+++ manimce/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $version = '0.3.0'
 
 . $toolsPath\helper.ps1
 
-$allowed_python_versions = @('3.6', '3.7', '3.8', '3.9') # sync with nuspec
+$allowed_python_versions = @('3.9', '3.8', '3.7', '3.6') # sync with nuspec
 
 $python = FindPython $allowed_python_versions
 Write-Host "Found python at '$python' using it."
