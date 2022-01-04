cecb239c8e98fc882f9297db289f5e8318151c10
Chocolatey <chocolatey@realdimensions.net>
2022-01-04 16:06:34 +0000

    AU: 3 updated - love love.install love.portable [skip ci] https://gist.github.com/64dce37e90a24ba0020575871cbb2b2f/299f45d298c83faf1a47282e40296b9da6f6c06d

--- love.portable/tools/chocolateyinstall.ps1
+++ love.portable/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  url           = 'https://github.com/love2d/love/releases/download/11.3/love-11.3-win32.zip'
-  url64bit      = 'https://github.com/love2d/love/releases/download/11.3/love-11.3-win64.zip'
-  checksum      = '6c94a62283dcc0f9927de0a07a8c00571b5f4c23de2779cbab3f2c63cfb8ce3c'
+  url           = 'https://github.com/love2d/love/releases/download/11.4/love-11.4-win32.zip'
+  url64bit      = 'https://github.com/love2d/love/releases/download/11.4/love-11.4-win64.zip'
+  checksum      = '0d0c42159c6a65bc23a70239171916f0900a2f08f3918e51065e6f1255f5494a'
   checksumType  = 'sha256'
-  checksum64    = 'eb3cdc9aef3a0ddf14a0f01b4ec2a93a37b8e26c52176291dfe79394a03f739c'
+  checksum64    = 'a03180c105557a70e922495ab21279faf327500586238288714d2e695bdc5e78'
   checksumType64= 'sha256'
 }
 
