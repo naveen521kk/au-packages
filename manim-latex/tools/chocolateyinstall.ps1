544c6b55dfa5fb27fb5e643ce8b5b40126c729e0
Kiran-Raj-Dev <90276965+Kiran-Raj-Dev@users.noreply.github.com>
2022-02-17 15:11:50 +0530

    manim-latex: added some more missing packages

--- manim-latex/tools/chocolateyinstall.ps1
+++ manim-latex/tools/chocolateyinstall.ps1
@@ -4,6 +4,6 @@ Start-ChocolateyProcessAsAdmin `
   -Statements "tlmgr update --self" `
   -Elevated
 Start-ChocolateyProcessAsAdmin `
-  -Statements "tlmgr install standalone everysel ctex frcursive preview doublestroke ms setspace rsfs relsize ragged2e fundus-calligra microtype wasysym physics dvisvgm jknapltx wasy cm-super babel-english gnu-freefont mathastext cbfonts-fd" `
+  -Statements "tlmgr install amsmath fontspec latex-bin tipa xcolor xetex xkeyval standalone everysel ctex frcursive preview doublestroke ms setspace rsfs relsize ragged2e fundus-calligra microtype wasysym physics dvisvgm jknapltx wasy cm-super babel-english gnu-freefont mathastext cbfonts-fd" `
   -Elevated
 
