ba2999051cde9fe7628255c946f93f59eedf3e44
Naveen M K <naveen521kk@gmail.com>
2022-12-02 23:35:47 +0530

    [AU scc:3.1.0]
    scc: fix install script
    didn't notice it for so long

--- scc/tools/chocolateyinstall.ps1
+++ scc/tools/chocolateyinstall.ps1
@@ -3,7 +3,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $zipArgs = @{
   Destination    = $toolsDir
-  FileFullPath   = "$(Get-Item $toolsDir\scc-*-i386-*.zip)"
-  FileFullPath64 = "$(Get-Item $toolsDir\scc-*-x86_64-*.zip)"
+  FileFullPath   = "$(Get-Item $toolsDir\scc*i386*)"
+  FileFullPath64 = "$(Get-Item $toolsDir\scc*x86_64*)"
 }
 Get-ChocolateyUnzip @zipArgs
+ 
