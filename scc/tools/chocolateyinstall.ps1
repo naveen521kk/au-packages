1a13ef50b9bf66bfe9fc1ac2858e9228cf6bfee7
Naveen M K <naveen521kk@gmail.com>
2022-12-27 10:01:33 -0800

    scc: extract the resulting file
    testing

--- scc/tools/chocolateyinstall.ps1
+++ scc/tools/chocolateyinstall.ps1
@@ -3,9 +3,14 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $zipArgs = @{
   Destination    = $toolsDir
-  FileFullPath   = "$(Get-Item $toolsDir\scc*i386*)"
-  FileFullPath64 = "$(Get-Item $toolsDir\scc*x86_64*)"
+  FileFullPath   = "$(Get-Item $toolsDir\scc_*i386*.gz)"
+  FileFullPath64 = "$(Get-Item $toolsDir\scc_*x86_64*.gz)"
 }
 
 Get-ChocolateyUnzip @zipArgs
  
+$zipArgs = @{
+  Destination    = $toolsDir
+  FileFullPath   = "$(Get-Item $toolsDir\scc*.tar)"
+}
+Get-ChocolateyUnzip @zipArgs
