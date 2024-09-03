4c8f762ea8f735c50020dc562eff96b1b58e7ab9
Naveen M K <naveen521kk@gmail.com>
2024-09-03 18:22:12 +0530

    [AU manim-latex:2024.09]

--- manim-latex/tools/chocolateyinstall.ps1
+++ manim-latex/tools/chocolateyinstall.ps1
@@ -8,5 +8,5 @@ $statementsToRun = "/C `"$toolsDir\TinyTeX\bin\windows\tlmgr.bat update --self`"
 Start-ChocolateyProcessAsAdmin $statementsToRun "$env:WINDIR\system32\cmd.exe"
 
 Write-Host "Installing extra packages"
-$statementsToRun = "/C `"$toolsDir\TinyTeX\bin\windows\tlmgr.bat install standalone everysel ctex frcursive preview doublestroke ms setspace rsfs relsize ragged2e fundus-calligra microtype wasysym physics dvisvgm jknapltx wasy cm-super babel-english gnu-freefont mathastext cbfonts-fd`""
+$statementsToRun = "/C `"$toolsDir\TinyTeX\bin\windows\tlmgr.bat install amsmath babel-english cbfonts-fd cm-super count1to ctex doublestroke dvisvgm everysel fontspec frcursive fundus-calligra gnu-freefont jknapltx latex-bin mathastext microtype multitoc physics preview prelim2e ragged2e relsize rsfs setspace standalone tipa wasy wasysym xcolor xetex xkeyval`""
 Start-ChocolateyProcessAsAdmin $statementsToRun "$env:WINDIR\system32\cmd.exe"
