9510711bc29cb9e6be3287f17f6c31ab77c41b9b
Naveen M K <naveen521kk@gmail.com>
2025-01-20 22:06:29 +0530

    [AU manimce]
    remove ffmpeg dep and add python 3.13

--- manimce/tools/chocolateyinstall.ps1
+++ manimce/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $version = '0.19.0'
 
 . $toolsPath\helper.ps1
 
-$allowed_python_versions = @('3.12', '3.11', '3.10', '3.9', '3.8') # sync with nuspec
+$allowed_python_versions = @('3.13', '3.12', '3.11', '3.10', '3.9') # sync with nuspec
 
 $python = FindPython $allowed_python_versions
 Write-Host "Found python at '$python' using it."
